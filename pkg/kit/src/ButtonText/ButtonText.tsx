9ee191b02ed61d099640a400868ddc51f608510d
Pierre Bertet <hello@pierre.world>
2021-07-31 15:45:59 +0100

    [kit] ButtonText: fix types

--- pkg/kit/src/ButtonText/ButtonText.tsx
+++ pkg/kit/src/ButtonText/ButtonText.tsx
@@ -1,6 +1,5 @@
 /** @jsx jsx */
-import type { ReactNode } from "react"
-import type { ButtonAreaProps } from "../ButtonArea"
+import type { ComponentPropsWithoutRef, ReactNode } from "react"
 
 import { createContext, forwardRef, useContext } from "react"
 import { jsx, css } from "@emotion/react"
@@ -11,42 +10,45 @@ export const ICON_SIZE_DEFAULT = 2.5 * gu
 
 const ButtonTextContext = createContext(false)
 
-type ButtonTextProps = ButtonAreaProps & {
-  icon?: ReactNode
-  label: ReactNode
-}
-
-export const ButtonText = forwardRef<HTMLButtonElement, ButtonTextProps>(
-  function ButtonText({ icon, label, ...props }, ref) {
-    return (
-      <ButtonArea
-        ref={ref}
-        {...props}
-        css={({ colors }) => css`
-          display: inline-flex;
-          align-items: center;
-          text-transform: uppercase;
-          font-size: 14px;
-          color: ${colors.content};
-          &:active {
-            transform: translate(1px, 1px);
-          }
-        `}
-      >
-        <ButtonTextContext.Provider value={true}>
-          {icon}
-          <span
-            css={css`
-              padding-left: ${icon ? css`1gu` : "0"};
-            `}
-          >
-            {label}
-          </span>
-        </ButtonTextContext.Provider>
-      </ButtonArea>
-    )
+type ButtonTextProps = ComponentPropsWithoutRef<"button"> &
+  ComponentPropsWithoutRef<"a"> & {
+    icon?: ReactNode
+    label: ReactNode
+    onClick?: () => void
   }
-)
+
+export const ButtonText = forwardRef<
+  HTMLButtonElement & HTMLAnchorElement,
+  ButtonTextProps
+>(function ButtonText({ icon, label, ...props }, ref) {
+  return (
+    <ButtonArea
+      ref={ref}
+      {...props}
+      css={({ colors }) => css`
+        display: inline-flex;
+        align-items: center;
+        text-transform: uppercase;
+        font-size: 14px;
+        color: ${colors.content};
+        &:active {
+          transform: translate(1px, 1px);
+        }
+      `}
+    >
+      <ButtonTextContext.Provider value={true}>
+        {icon}
+        <span
+          css={css`
+            padding-left: ${icon ? css`1gu` : "0"};
+          `}
+        >
+          {label}
+        </span>
+      </ButtonTextContext.Provider>
+    </ButtonArea>
+  )
+})
 
 export function useInsideButtonText() {
   return useContext(ButtonTextContext)
