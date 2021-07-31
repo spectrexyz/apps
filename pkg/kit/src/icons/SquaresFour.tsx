16d66820dd7054b6ea32e062ce2edf35d14e7f46
Pierre Bertet <hello@pierre.world>
2021-07-31 15:47:46 +0100

    [kit] icons: add SquaresFour (#59)

---
+++ pkg/kit/src/icons/SquaresFour.tsx
@@ -0,0 +1,26 @@
+import type { SVGProps } from "react"
+import React from "react"
+import { useIconSize, useIconColor } from "../icons-utils"
+type IconSquaresFourProps = SVGProps<SVGSVGElement> & {
+  color?: string,
+  size?: number,
+}
+export default function IconSquaresFour({
+  color,
+  size,
+  ...props
+}: IconSquaresFourProps) {
+  color = useIconColor(color)
+  size = useIconSize(size)
+  return (
+    <svg width={size} height={size} viewBox="0 0 32 32" fill="none" {...props}>
+      <path
+        d="M14 6H6v8h8V6zM26 6h-8v8h8V6zM14 18H6v8h8v-8zM26 18h-8v8h8v-8z"
+        stroke={color}
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
