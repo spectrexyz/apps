1d711ff0c5b0caa9f53292ce504cf0a651588b34
Pierre Bertet <hello@pierre.world>
2021-11-19 15:57:23 +0000

    kit: ESLint fixes (#79)

--- pkg/kit/src/Root/Root.tsx
+++ pkg/kit/src/Root/Root.tsx
@@ -1,26 +1,19 @@
 import type { ReactNode } from "react"
 
-import { createContext, useCallback, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react"
 import ReactDOM from "react-dom"
 
-const RootContext = createContext(null)
+const RootContext = createContext<HTMLDivElement | null>(null)
 
 type RootEntryPointProps = {
   children: ReactNode
 }
 
 export function RootEntryPoint({ children }: RootEntryPointProps): JSX.Element {
-  const [element, setElement] = useState(null)
-
-  const ref = useCallback((element) => {
-    if (element !== null) {
-      setElement(element)
-    }
-  }, [])
-
+  const [element, setElement] = useState<HTMLDivElement | null>(null)
   return (
     <RootContext.Provider value={element}>
-      <div ref={ref}>{element ? children : null}</div>
+      <div ref={setElement}>{element ? children : null}</div>
     </RootContext.Provider>
   )
 }
---
+++ pkg/kit/src/ogl.d.ts
@@ -0,0 +1,47 @@
+declare module "ogl" {
+  interface Gl {
+    canvas: HTMLCanvasElement
+  }
+
+  type Uniforms = Record<string, { value: unknown }>
+
+  class Renderer {
+    constructor(options: { dpr: number; alpha: boolean })
+    setSize(width: number, height: number): void
+    render({ scene: Mesh }): void
+    gl: Gl
+  }
+
+  class Mesh {
+    constructor(
+      gl: Gl,
+      options: {
+        geometry: Box
+        program: Program
+      }
+    )
+  }
+
+  class Program {
+    constructor(
+      gl: Gl,
+      options: {
+        transparent: boolean
+        fragment: string
+        vertex: string
+        uniforms: Uniforms
+      }
+    )
+    uniforms: Uniforms
+  }
+
+  class Box {
+    constructor(
+      gl: Gl,
+      options: {
+        width: number
+        height: number
+      }
+    )
+  }
+}
