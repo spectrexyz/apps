14bc5b1955f62c6d77184eb12ed8213d10035b08
Pierre Bertet <hello@pierre.world>
2021-11-22 20:08:52 +0000

    kit: Steps: auto width

--- pkg/kit/src/Steps/Steps.tsx
+++ pkg/kit/src/Steps/Steps.tsx
@@ -12,6 +12,10 @@ type StepsProps = {
   steps: number
 }
 
+const NUMBER_SIZE = 3 * gu
+const BAR_THICKNESS = 0.5 * gu
+const BAR_SPACING = 1.75 * gu
+
 export function Steps({
   current,
   direction = "horizontal",
@@ -23,33 +27,55 @@ export function Steps({
       `Steps: the number of steps should be greater than 1 (received ${steps}).`
     )
   }
+
+  const bounds = useDimensions()
+
+  // Bar length = its width or height, depending on direction
+  const barLength =
+    ((direction === "vertical" ? bounds.height : bounds.width) -
+      NUMBER_SIZE * steps) /
+      (steps - 1) -
+    BAR_SPACING * 2
+
   return (
     <div
+      ref={bounds.observe}
       css={css`
         display: flex;
-        flex-direction: ${direction === "horizontal" ? "row" : "column"};
-        justify-content: space-between;
-        align-items: center;
-        width: ${direction === "horizontal" ? "100%" : css`8gu`};
-        height: ${direction === "horizontal" ? css`8gu` : "100%"};
-        padding: ${direction === "horizontal" ? css`0 2gu` : css`2gu 0`};
+        overflow: hidden;
       `}
     >
-      {Array.from({ length: steps * 2 - 1 }).map((_, index) => {
-        const bar = index % 2 === 1
-        const step = index - Math.floor(index / 2)
-        const active = step <= current
-        return bar ? (
-          <StepBar key={index} active={active} direction={direction} />
-        ) : (
-          <StepNumber
-            key={index}
-            active={active}
-            checked={step < current}
-            label={String(step + 1)}
-          />
-        )
-      })}
+      <div
+        css={css`
+          display: flex;
+          flex-direction: ${direction === "horizontal" ? "row" : "column"};
+          justify-content: space-between;
+          align-items: center;
+          width: ${direction === "horizontal" ? "auto" : `${NUMBER_SIZE}px`};
+          height: ${direction === "horizontal" ? `${NUMBER_SIZE}px` : "auto"};
+        `}
+      >
+        {Array.from({ length: steps * 2 - 1 }).map((_, index) => {
+          const bar = index % 2 === 1
+          const step = index - Math.floor(index / 2)
+          const active = step <= current
+          return bar ? (
+            <StepBar
+              key={index}
+              active={active}
+              direction={direction}
+              length={barLength}
+            />
+          ) : (
+            <StepNumber
+              key={index}
+              active={active}
+              checked={step < current}
+              label={String(step + 1)}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
@@ -103,8 +129,8 @@ function StepNumber({
         display: flex;
         align-items: center;
         justify-content: center;
-        width: 3gu;
-        height: 3gu;
+        width: ${NUMBER_SIZE}px;
+        height: ${NUMBER_SIZE}px;
         font-family: ${fonts.families.sans};
         font-weight: 800;
         font-size: 15px;
@@ -170,12 +196,13 @@ function StepNumber({
 function StepBar({
   active,
   direction,
+  length,
 }: {
   active: boolean
   direction: "horizontal" | "vertical"
+  length: number
 }) {
   const { colors } = useTheme()
-  const bounds = useDimensions()
   const visibility = useSpring({
     config: springs.lazy,
     transform:
@@ -185,11 +212,15 @@ function StepBar({
   })
   return (
     <div
-      ref={bounds.observe}
       css={css`
         display: flex;
-        width: ${direction === "horizontal" ? "100%" : css`0.5gu`};
-        height: ${direction === "horizontal" ? css`0.5gu` : "100%"};
+        flex-shrink: 1;
+        width: ${direction === "horizontal"
+          ? `${length}px`
+          : `${BAR_THICKNESS}px`};
+        height: ${direction === "horizontal"
+          ? `${BAR_THICKNESS}px`
+          : `${length}px`};
         background: ${colors.accentInverted};
         border-radius: 0.25gu;
         margin: ${direction === "horizontal" ? css`0 1.75gu` : css`1.75gu 0`};
@@ -205,10 +236,10 @@ function StepBar({
         <Moire
           animate={true}
           backgroundColor={colors.accentInverted}
-          height={bounds.height}
+          height={direction === "horizontal" ? BAR_THICKNESS : length}
           linesColor={colors.accentInvertedContent}
-          width={bounds.width}
           scale={0.75}
+          width={direction === "vertical" ? BAR_THICKNESS : length}
         />
       </a.div>
     </div>
