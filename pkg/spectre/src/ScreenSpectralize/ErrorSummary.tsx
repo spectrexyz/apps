7af82034518ae3cca1fba811cfa2878bdb4e1f18
Pierre Bertet <hello@pierre.world>
2022-01-20 15:19:40 +0000

    spectre: add ErrorSummary

---
+++ pkg/spectre/src/ScreenSpectralize/ErrorSummary.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react"
+import { css } from "@emotion/react"
+
+export function ErrorSummary({ children }: { children?: ReactNode }) {
+  return children ? (
+    <div
+      css={({ colors, fonts }) => css`
+        margin-top: 2gu;
+        padding: 1gu 2gu;
+        font-family: ${fonts.families.sans};
+        font-size: 14px;
+        color: ${colors.warning};
+        background: ${colors.warningSurface};
+        border-radius: 6px;
+
+        ul,
+        p {
+          margin: 1gu 0;
+        }
+        ul {
+          list-style-position: inside;
+        }
+      `}
+    >
+      {children}
+    </div>
+  ) : null
+}
