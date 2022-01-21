ebaf7f9f6ce707daa6bf984dc39caded88a41d8f
Pierre Bertet <hello@pierre.world>
2022-01-21 15:01:22 +0000

    kit: add secondaryStart / secondaryEnd to TokenInput

--- pkg/demo/src/TokenInput.tsx
+++ pkg/demo/src/TokenInput.tsx
@@ -1,31 +1,83 @@
 import { useState } from "react"
 import { css } from "@emotion/react"
-import { TokenInput as KitTokenInput } from "kit"
+import { TokenAmount, TokenInput as KitTokenInput, usePrice } from "kit"
 
 export function TokenInput() {
-  const [value, setValue] = useState(1)
   return (
     <div
       css={css`
-        padding-top: 4gu;
+        display: grid;
+        gap: 2gu;
       `}
     >
-      <KitTokenInput
-        balance="106.970"
-        balanceConverted="$283,982"
-        onChange={(value) => {
-          const _value = value.trim()
-          if (_value === "") {
-            setValue(-1)
-            return
-          }
-
-          const valueNum = parseFloat(_value)
-          if (!isNaN(valueNum)) setValue(valueNum)
-        }}
-        symbol="ETH"
-        value={value === -1 ? "" : String(value)}
-      />
+      <TokenInput1 />
+      <TokenInput2 />
     </div>
+  )
+}
+
+function eth(amount: bigint) {
+  return new TokenAmount(amount, 18, { symbol: "ETH" })
+}
+
+function usd(amount: bigint) {
+  return new TokenAmount(amount, 18, { symbol: "USD" })
+}
+
+function TokenInput1() {
+  const [value, setValue] = useState<null | TokenAmount>(
+    eth(1000000000000000000n)
+  )
+  const ethPrice = usePrice("eth", "usd")
+  const balanceEth = eth(106970000000000000000n)
+  const balanceUsd = ethPrice.data
+    ? balanceEth.convert(ethPrice.data, 18)
+    : null
+
+  return (
+    <KitTokenInput
+      balance={`${balanceEth.format({ symbol: undefined })}`}
+      balanceConverted={balanceUsd ? `$${balanceUsd.format()}` : "−"}
+      onBalanceClick={() => setValue(balanceEth)}
+      onChange={(value) => {
+        const _value = value.trim()
+        if (_value === "") {
+          setValue(null)
+          return
+        }
+
+        const valueNum = parseFloat(_value)
+        if (!isNaN(valueNum)) {
+          setValue(eth(BigInt(valueNum * 10 ** 18)))
+        }
+      }}
+      symbol="ETH"
+      value={value === null ? "" : value.format({ symbol: undefined, commify: false })}
+    />
+  )
+}
+
+function TokenInput2() {
+  const [value, setValue] = useState(1)
+  const ethPrice = usePrice("eth", "usd")
+  const usdValue = ethPrice.data
+    ? usd(BigInt(ethPrice.data * 10 ** 18 * value))
+    : null
+  return (
+    <KitTokenInput
+      secondaryEnd={value === -1 ? "" : usdValue?.format()}
+      onChange={(value) => {
+        const _value = value.trim()
+        if (_value === "") {
+          setValue(-1)
+          return
+        }
+
+        const valueNum = parseFloat(_value)
+        if (!isNaN(valueNum)) setValue(valueNum)
+      }}
+      symbol="ETH"
+      value={value === -1 ? "" : String(value)}
+    />
   )
 }
--- pkg/kit/src/TokenInput/TokenInput.tsx
+++ pkg/kit/src/TokenInput/TokenInput.tsx
@@ -1,6 +1,4 @@
-import type { ChangeEvent } from "react"
-
-import { useCallback } from "react"
+import { ChangeEvent, ReactNode, useCallback } from "react"
 import { css } from "@emotion/react"
 import { gu } from "../styles"
 import { Button } from "../Button"
@@ -10,7 +8,10 @@ type TokenInputProps = {
   balance?: string
   balanceConverted?: string
   maxButton?: boolean
+  onBalanceClick?: () => void
   onChange: (value: string) => void
+  secondaryEnd?: ReactNode
+  secondaryStart?: ReactNode
   symbol: string
   value: string
 }
@@ -19,10 +20,23 @@ export function TokenInput({
   balance,
   balanceConverted,
   maxButton = false,
+  onBalanceClick,
   onChange,
+  secondaryEnd,
+  secondaryStart,
   symbol,
   value,
 }: TokenInputProps): JSX.Element {
+  const hasBalanceRow = balance !== undefined || balanceConverted !== undefined
+  const hasSecondaryRow =
+    secondaryStart !== undefined || secondaryEnd !== undefined
+
+  if (hasBalanceRow && hasSecondaryRow) {
+    throw new Error(
+      "TokenInput: please only use balance / balanceConverted or secondary, not both."
+    )
+  }
+
   const handleChange = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
       const value = event.currentTarget.value.trim()
@@ -32,6 +46,7 @@ export function TokenInput({
     },
     [onChange]
   )
+
   return (
     <div>
       <div
@@ -91,35 +106,57 @@ export function TokenInput({
           `}
         />
       </div>
-      {balance !== undefined && (
-        <div
-          css={({ fonts }) => css`
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            height: 3gu;
-            font-family: ${fonts.families.sans};
-          `}
-        >
-          <div
-            css={({ colors }) => css`
-              font-size: 12px;
-              span {
-                color: ${colors.contentDimmed};
-              }
-            `}
-          >
-            <span>Balance:</span> {balance} {symbol}
-          </div>
-          <div
-            css={css`
-              font-size: 14px;
-            `}
-          >
-            {balanceConverted}
-          </div>
-        </div>
+      {hasSecondaryRow && (
+        <SecondaryRow end={secondaryEnd} start={secondaryStart} />
       )}
+      {hasBalanceRow && (
+        <SecondaryRow
+          end={balanceConverted}
+          start={
+            <span
+              onClick={onBalanceClick}
+              css={css`
+                cursor: ${onBalanceClick ? "pointer" : "default"};
+                user-select: none;
+              `}
+            >
+              <span>Balance:</span> {balance} {symbol}
+            </span>
+          }
+        />
+      )}
+    </div>
+  )
+}
+
+function SecondaryRow({ start, end }: { start?: ReactNode; end?: ReactNode }) {
+  return (
+    <div
+      css={({ fonts }) => css`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 3gu;
+        font-family: ${fonts.families.sans};
+      `}
+    >
+      <div
+        css={({ colors }) => css`
+          font-size: 12px;
+          span {
+            color: ${colors.contentDimmed};
+          }
+        `}
+      >
+        {start}
+      </div>
+      <div
+        css={css`
+          font-size: 14px;
+        `}
+      >
+        {end}
+      </div>
     </div>
   )
 }
