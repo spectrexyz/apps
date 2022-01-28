aebdc72bf72eb824caa0c220e2ba70b6ca6d26b2
Pierre Bertet <hello@pierre.world>
2022-01-28 22:35:06 +0000

    spectre: dedupe dependencies in the production build

--- pkg/spectre/vite.config.js
+++ pkg/spectre/vite.config.js
@@ -3,16 +3,31 @@ import checker from "vite-plugin-checker"
 // import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(async ({ mode }) => ({
   build: {
     target: "es2020",
+    outDir: "dist",
+    sourcemap: mode === "production" || "inline",
   },
   esbuild: {
     jsxFactory: "jsx",
     jsxInject: `import { jsx } from '@emotion/react'`,
   },
+  optimizeDeps: {
+    entries: ["./src/index.tsx"],
+  },
   plugins: [
     // react(),
     checker({ typescript: true }),
   ],
-})
+  resolve: {
+    dedupe: [
+      "@emotion/cache",
+      "@emotion/react",
+      "react",
+      "react-dom",
+      "react-spring",
+      "stylis",
+    ],
+  },
+}))
