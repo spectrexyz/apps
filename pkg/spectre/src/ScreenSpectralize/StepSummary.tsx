b429b6ee43f99e105eb6ef3c7369d60918c4e3ed
Pierre Bertet <hello@pierre.world>
2022-01-28 17:40:15 +0000

    spectre: add StepSummary (spectralize)

---
+++ pkg/spectre/src/ScreenSpectralize/StepSummary.tsx
@@ -0,0 +1,263 @@
+import React, { useCallback } from "react"
+import { css } from "@emotion/react"
+import {
+  AddressBadge,
+  Button,
+  Definition,
+  formatAmount,
+  formatBytes,
+  formatNumber,
+} from "kit"
+import {
+  ContentLayout,
+  ContentLayoutHeading,
+  ContentLayoutSection,
+} from "../ContentLayout"
+import { useLayout } from "../styles"
+import { AdvancedParameters } from "./AdvancedParameters"
+import { StepProps } from "./types"
+import { useSpectralize } from "./use-spectralize"
+
+export function StepSummary({ title, onNext, onPrev }: StepProps) {
+  const data = useSpectralize()
+  const layout = useLayout()
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault()
+      onNext()
+    },
+    [onNext]
+  )
+
+  const headingBottomSpace = layout.value({
+    small: css`1gu`,
+    large: css`5.25gu`,
+  })
+
+  const fractionalize = () => {
+    alert("Soon")
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      css={css`
+        width: 100%;
+      `}
+    >
+      <ContentLayout>
+        <ContentLayoutHeading
+          title={
+            <span
+              css={({ colors }) => css`
+                color: ${colors.accent};
+              `}
+            >
+              {title}
+            </span>
+          }
+        >
+          Make sure all the information related to your NFT & fractions is
+          correct before you confirm.
+        </ContentLayoutHeading>
+        <div
+          css={css`
+            height: ${headingBottomSpace};
+          `}
+        />
+        <ContentLayoutSection type="two-parts">
+          <div
+            css={css`
+              padding-bottom: 3gu;
+            `}
+          >
+            {data.previewUrl && (
+              <img
+                src={data.previewUrl}
+                alt=""
+                css={css`
+                  display: block;
+                  width: 100%;
+                `}
+              />
+            )}
+
+            <div
+              css={css`
+                display: grid;
+                gap: 2gu;
+                grid-template-columns: repeat(2, 1fr);
+                width: 100%;
+                padding: 3gu 0;
+              `}
+            >
+              <Definition title="NFT Title" content={data.title} />
+              <Definition
+                title={`Artwork details (${data.fileType})`}
+                content={<Truncate text={artworkDetails(data.file)} />}
+              />
+            </div>
+            <Definition title="NFT Description" content={data.description} />
+          </div>
+          <div>
+            <div
+              css={css`
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+                gap: 3gu;
+                padding-bottom: 3gu;
+              `}
+            >
+              <Definition
+                title="Token Name"
+                content={<Truncate text={data.tokenName} />}
+              />
+              <Definition
+                title="Token Symbol"
+                content={<Truncate text={data.tokenSymbol} />}
+              />
+              <Definition
+                title="Initial Token Price"
+                content={
+                  <Truncate
+                    text={`${formatAmount(data.initialTokenPrice, 18)} ETH`}
+                  />
+                }
+              />
+              <Definition
+                title="Initial Market Cap"
+                content={`${formatAmount(data.totalMarketCap, 18)} ETH`}
+              />
+              <Definition
+                title="Max Token Supply Cap"
+                content={formatNumber(data.maxTokenSupplyCap)}
+              />
+              <Definition
+                title="Buyout Multiplier"
+                content={`${data.buyoutMultiplier / 10}x`}
+              />
+              <Definition
+                title="Buyout Price"
+                content={`${formatAmount(data.nftBuyoutPrice, 18)} ETH`}
+              />
+              <Definition
+                title="Minting Rewards"
+                content={`${formatNumber(data.rewardsPct)}%`}
+              />
+            </div>
+            <Definition
+              title="Allocation of Minting Rewards"
+              content={
+                <div
+                  css={css`
+                    display: flex;
+                    flex-direction: column;
+                    gap: 1.5gu;
+                    padding-top: 0.5gu;
+                  `}
+                >
+                  {data.rewardsSplit.map((account) => (
+                    <div
+                      key={account}
+                      css={css`
+                        display: flex;
+                        gap: 1.5gu;
+                      `}
+                    >
+                      <AddressBadge address={account} />
+                      <div
+                        css={css`
+                          display: flex;
+                          align-items: center;
+                        `}
+                      >
+                        {formatNumber(
+                          data.rewardsPct / data.rewardsSplit.length
+                        )}
+                        %
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              }
+            />
+            <div
+              css={css`
+                padding-top: 3gu;
+              `}
+            >
+              <AdvancedParameters headingFontSize="14px" />
+            </div>
+          </div>
+        </ContentLayoutSection>
+        <div>
+          {layout.below("medium") ? (
+            <div
+              css={css`
+                padding: 3gu 0;
+              `}
+            >
+              <Button
+                label="Fractionalize NFT"
+                mode="primary-2"
+                onClick={fractionalize}
+                shadowInBox
+                type="submit"
+                wide
+              />
+            </div>
+          ) : (
+            <div
+              css={css`
+                display: flex;
+                justify-content: flex-end;
+                gap: 2gu;
+                padding-top: 3gu;
+              `}
+            >
+              <Button
+                label="Back"
+                mode="secondary-2"
+                shadowInBox
+                onClick={onPrev}
+              />
+              <Button
+                label="Fractionalize NFT"
+                mode="primary-2"
+                onClick={fractionalize}
+                shadowInBox
+                type="submit"
+              />
+            </div>
+          )}
+        </div>
+      </ContentLayout>
+    </form>
+  )
+}
+
+function Truncate({ text }: { text: string }) {
+  return (
+    <div
+      title={text}
+      css={css`
+        max-width: 100%;
+        white-space: nowrap;
+        text-align: left;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      `}
+    >
+      {text}
+    </div>
+  )
+}
+
+function artworkDetails(file: File | null): string {
+  let details = file?.name ?? ""
+  if (file?.size) {
+    details += ` − ${formatBytes(file?.size)}`
+  }
+  return details
+}
