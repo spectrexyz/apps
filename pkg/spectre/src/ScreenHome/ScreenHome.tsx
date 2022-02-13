2dfcf06a62afea6b49c9213a1b6cd2e96a6dc98f
Pierre Bertet <hello@pierre.world>
2022-02-13 19:13:17 +0000

    spectre: move to css objects

--- pkg/spectre/src/ScreenHome/ScreenHome.tsx
+++ pkg/spectre/src/ScreenHome/ScreenHome.tsx
@@ -1,4 +1,3 @@
-import { css } from "@emotion/react"
 import { Button } from "kit"
 import { useLocation } from "wouter"
 import { AppScreen } from "../AppLayout/AppScreen2"
@@ -10,24 +9,24 @@ export function ScreenHome() {
   return (
     <AppScreen compactBar={null}>
       <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          height: 100%;
-          h1 {
-            padding-bottom: 5gu;
-          }
-        `}
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+          "h1": {
+            paddingBottom: "5gu",
+          },
+        }}
       >
         <h1>Spectre</h1>
 
         <div
-          css={css`
-            display: grid;
-            gap: 2gu;
-          `}
+          css={{
+            display: "grid",
+            gap: "2gu",
+          }}
         >
           <Button
             label="NFT"
@@ -53,24 +52,6 @@ export function ScreenHome() {
             wide
           />
         </div>
-        {
-          /*[
-          // ["NFT #1", "/nfts/1"],
-          // ["TOKEN #1", "/nfts/1/serc20"],
-          // ["Buy token", "/nfts/1/buy"],
-        ].map(([label, path]) => (
-          <div
-            key={path}
-            css={css`
-              & + & {
-                padding-top: 2gu;
-              }
-            `}
-          >
-            <Button label={label} onClick={() => setLocation(path)} />
-          </div>
-        ))*/
-        }
       </div>
     </AppScreen>
   )
