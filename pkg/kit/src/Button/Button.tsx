27bf56b41d2de74fa651836d5c441dfe513bcf21
Pierre Bertet <hello@pierre.world>
2022-02-16 15:02:31 +0000

    kit: add uppercase prop to Button

--- pkg/kit/src/Button/Button.tsx
+++ pkg/kit/src/Button/Button.tsx
@@ -1,6 +1,5 @@
 import type { ComponentPropsWithoutRef, ReactNode } from "react"
 
-import { css } from "@emotion/react"
 import { createContext, forwardRef, useContext, useMemo } from "react"
 import useDimensions from "react-cool-dimensions"
 import { ButtonArea } from "../ButtonArea"
@@ -50,6 +49,7 @@ export type ButtonProps =
     // wether the shadow should be part of the button box or not
     shadowInBox?: ShadowInBox
     size?: ButtonSize
+    uppercase?: boolean
     wide?: boolean
   }
 
@@ -69,6 +69,7 @@ export const Button = forwardRef<
     onClick,
     shadowInBox = false,
     size = "medium",
+    uppercase = false,
     wide = false,
     ...props
   },
@@ -89,6 +90,17 @@ export const Button = forwardRef<
     return "18px"
   }, [size])
 
+  const { colors } = useTheme()
+  const color = useMemo(() => {
+    if (mode === "flat") return colors.accent
+    if (mode === "flat-2") return colors.accent
+    if (mode === "flat-3") return colors.accent
+    if (mode === "primary") return colors.accentContent
+    if (mode === "primary-2") return colors.accent2Content
+    if (mode === "secondary-2") return colors.accent2
+    return colors.accent // secondary
+  }, [mode])
+
   return (
     <ButtonContext.Provider value={{ size }}>
       <ButtonArea
@@ -99,67 +111,43 @@ export const Button = forwardRef<
         onClick={onClick}
         type={href ? undefined : "button"}
         {...props}
-        css={({ colors, fonts }) =>
-          css`
-          display: inline-flex;
-          align-items: center;
-          justify-content: center;
-          width: ${wide ? "100%" : "auto"};
-          height: ${heightBase + (vShift ? SHADOW_OFFSET : 0)}px;
-          padding-right: ${hShift ? SHADOW_OFFSET : 0}px;
-          padding-bottom: ${vShift ? SHADOW_OFFSET : 0}px;
-          text-decoration: none;
-          white-space: nowrap;
-          font-size: ${fontSize};
-          font-family: ${fonts.mono};
-          color: ${
-            (() => {
-              if (mode === "flat") return colors.accent
-              if (mode === "flat-2") return colors.accent
-              if (mode === "flat-3") return colors.accent
-              if (mode === "primary") return colors.accentContent
-              if (mode === "primary-2") return colors.accent2Content
-              if (mode === "secondary-2") return colors.accent2
-              return colors.accent // secondary
-            })()
-          };
-          &:focus-visible {
-            outline: 0;
-            .surface {
-              outline: 2px solid ${colors.focus};
-              outline-offset: -1px;
-            }
-            .shadow {
-              opacity: 0;
-            }
-            .active-shadow {
-              opacity: 1;
-              background: ${colors.focus};
-            }
-          }
-          &:active {
-            .surface {
-              transform: translate(1.5px, 1.5px);
-            }
-            .shadow {
-              opacity: 0;
-            }
-            .active-shadow {
-              opacity: 1;
-            }
-            &:disabled {
-              .surface {
-                transform: none;
-              }
-              .active-shadow {
-                opacity: 0;
-              }
-              .shadow {
-                opacity: 1;
-              }
-            }
-          }
-        `}
+        css={({ colors, fonts }) => ({
+          display: "inline-flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: wide ? "100%" : "auto",
+          height: `${heightBase + (vShift ? SHADOW_OFFSET : 0)}px`,
+          paddingRight: `${hShift ? SHADOW_OFFSET : 0}px`,
+          paddingBottom: `${vShift ? SHADOW_OFFSET : 0}px`,
+          textDecoration: "none",
+          whiteSpace: "nowrap",
+          fontSize: fontSize,
+          fontFamily: fonts.mono,
+          textTransform: uppercase ? "uppercase" : "none",
+          color,
+          "&:focus-visible": {
+            outline: 0,
+            ".surface": {
+              outline: `2px solid ${colors.focus}`,
+              outlineOffset: "-1px",
+            },
+            ".shadow": { opacity: 0 },
+            ".active-shadow": {
+              opacity: 1,
+              background: colors.focus,
+            },
+          },
+          "&:active": {
+            ".surface": { transform: "translate(1.5px, 1.5px)" },
+            ".shadow": { opacity: 0 },
+            ".active-shadow": { opacity: 1 },
+            "&:disabled": {
+              ".surface": { transform: "none" },
+              ".active-shadow": { opacity: 0 },
+              ".shadow": { opacity: 1 },
+            },
+          },
+        })}
       >
         <ButtonIn
           adjustLabelAlignment={adjustLabelAlignment}
@@ -200,109 +188,105 @@ function ButtonIn({
     return 3 * gu
   }, [size, horizontalPadding])
 
+  const background = useMemo(() => {
+    if (mode === "flat") return colors.layer2
+    if (mode === "flat-2") return colors.layer1
+    if (mode === "flat-3") return colors.background
+    if (mode === "primary") return colors.accent
+    if (mode === "primary-2") return colors.accent2
+    return colors.background // secondary, secondary-2
+  }, [mode])
+
+  const borderColor = useMemo(() => {
+    if (mode === "primary-2") return colors.accent2
+    if (mode === "secondary-2") return colors.accent2
+    return colors.accent
+  }, [mode])
+
   return (
     <div
-      css={css`
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-      `}
+      css={{
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+      }}
     >
       <div
         className="surface"
-        css={({ colors }) =>
-          css`
-          position: relative;
-          z-index: 2;
-          display: flex;
-          align-items: center;
-          width: 100%;
-          height: 100%;
-          padding: 0 ${_horizontalPadding}px;
-          background: ${
-            (() => {
-              if (mode === "flat") return colors.layer2
-              if (mode === "flat-2") return colors.layer1
-              if (mode === "flat-3") return colors.background
-              if (mode === "primary") return colors.accent
-              if (mode === "primary-2") return colors.accent2
-              return colors.background // secondary, secondary-2
-            })()
-          };
-          border-style: solid;
-          border-width: ${flat ? "0" : "1px"};
-          border-color: ${
-            (() => {
-              if (mode === "primary-2") return colors.accent2
-              if (mode === "secondary-2") return colors.accent2
-              return colors.accent
-            })()
-          };
-        `}
+        css={{
+          position: "relative",
+          zIndex: "2",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          padding: `0 ${_horizontalPadding}px`,
+          background,
+          borderStyle: "solid",
+          borderWidth: flat ? "0" : "1px",
+          borderColor,
+        }}
       >
         {icon && (
           <div
-            css={css`
-              display: flex;
-              align-items: center;
-              height: 100%;
-              padding-right: 1gu;
-            `}
+            css={{
+              display: "flex",
+              alignItems: "center",
+              height: "100%",
+              paddingRight: "1gu",
+            }}
           >
             {icon}
           </div>
         )}
         <div
-          css={css`
-            position: relative;
-            top: ${adjustLabelAlignment ? "-1px" : "0"};
-            flex-grow: 1;
-            text-align: center;
+          css={{
+            position: "relative",
+            top: adjustLabelAlignment ? "-1px" : "0",
+            flexGrow: "1",
+            textAlign: "center",
 
-            /* ellipsis */
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-          `}
+            // ellipsis
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+          }}
         >
           {label}
         </div>
       </div>
       {!flat && mode !== "outline" && (
         <div
-          css={css`
-            position: absolute;
-            z-index: 1;
-            inset: 0;
-            transform: translate(${SHADOW_OFFSET}px, ${SHADOW_OFFSET}px);
-            pointer-events: none;
-          `}
+          css={{
+            position: "absolute",
+            zIndex: "1",
+            inset: "0",
+            transform: `translate(${SHADOW_OFFSET}px, ${SHADOW_OFFSET}px)`,
+            pointerEvents: "none",
+          }}
         >
           <div
             className="active-shadow"
-            css={({ colors }) =>
-              css`
-              position: absolute;
-              inset: 0;
-              background: ${
-                mode === "primary-2" || mode === "secondary-2"
-                  ? colors.accent2
-                  : colors.accent
-              };
-              opacity: 0;
-            `}
+            css={({ colors }) => ({
+              position: "absolute",
+              inset: "0",
+              background: mode === "primary-2" || mode === "secondary-2"
+                ? colors.accent2
+                : colors.accent,
+              opacity: "0",
+            })}
           />
           <div
             ref={shadowBounds.observe}
             className="shadow"
-            css={css`
-              position: absolute;
-              inset: 0;
-              overflow: hidden;
-            `}
+            css={{
+              position: "absolute",
+              inset: "0",
+              overflow: "hidden",
+            }}
           >
             <Moire
               width={Math.ceil(shadowBounds.width)}
