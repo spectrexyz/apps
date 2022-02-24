8e48fcbc10bfd3765b15c8fd17a3e933fa22c14b
Pierre Bertet <hello@pierre.world>
2022-02-24 18:23:20 +0000

    kit: fix Distribution tests

--- pkg/kit/test/Distribution/Distribution.test.tsx
+++ pkg/kit/test/Distribution/Distribution.test.tsx
@@ -1,19 +1,19 @@
 import { describe, expect, it } from "vitest"
-import { fillDistribution } from "../../src/Distribution/Distribution.tsx"
+import { correctDistribution } from "../../src/Distribution/Distribution"
 
-describe("Distribution: fillDistribution()", () => {
+describe("Distribution: correctDistribution()", () => {
   it("doesn’t touch already filled arrays", () => {
-    expect(fillDistribution([100])).toEqual([100])
+    expect(correctDistribution([100])).toEqual([100])
   })
   it("fills empty arrays", () => {
-    expect(fillDistribution([])).toEqual([100])
+    expect(correctDistribution([])).toEqual([100])
   })
   it("fills missing values", () => {
-    expect(fillDistribution([10, 10, 30])).toEqual([10, 10, 30, 50])
+    expect(correctDistribution([10, 10, 30])).toEqual([10, 10, 30, 50])
   })
   it("cuts extra values", () => {
-    expect(fillDistribution([10, 10, 30, 60])).toEqual([10, 10, 30, 50])
-    expect(fillDistribution([10, 10, 30, 600])).toEqual([10, 10, 30, 50])
-    expect(fillDistribution([10, 10, 90, 600])).toEqual([10, 10, 80])
+    expect(correctDistribution([10, 10, 30, 60])).toEqual([10, 10, 30, 50])
+    expect(correctDistribution([10, 10, 30, 600])).toEqual([10, 10, 30, 50])
+    expect(correctDistribution([10, 10, 90, 600])).toEqual([10, 10, 80])
   })
 })
