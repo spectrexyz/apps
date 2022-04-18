ac4ab234769755f8f1d03a2e0e888b3b6240b1c5
Pierre Bertet <hello@pierre.world>
2022-04-18 02:22:22 +0100

    spectre: move to React 18

--- pkg/spectre/src/main.tsx
+++ pkg/spectre/src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import { App } from "./App/App"
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root"),
-)
+const elt = document.getElementById("root")
+
+if (elt) {
+  createRoot(elt).render(<App />)
+}
