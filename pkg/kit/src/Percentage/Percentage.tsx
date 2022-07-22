b992fb9c93cc7fafc8437344f2aca5169228bda9
Pierre Bertet <hello@pierre.world>
2022-07-22 18:43:18 +0100

    kit: accept Dnum values on Percentage

--- pkg/kit/src/Percentage/Percentage.tsx
+++ pkg/kit/src/Percentage/Percentage.tsx
@@ -1,11 +1,34 @@
+import type { Dnum } from "dnum"
+
+import dnum from "dnum"
 import { memo } from "react"
 
+function isDnum(value: unknown): value is Dnum {
+  return Array.isArray(value)
+    && value.length >= 2
+    && typeof value[0] === "bigint"
+    && typeof value[1] === "number"
+}
+
 export const Percentage = memo(
-  function Percentage({ percentage }: { percentage: string | number }) {
+  function Percentage({
+    percentage,
+  }: {
+    percentage: Dnum | string | number
+  }) {
+    if (isDnum(percentage)) {
+      percentage = dnum.format(dnum.multiply(percentage, 100))
+    }
+
     let [whole, fraction = null] = String(percentage).split(".")
     if (fraction !== null) {
-      fraction = Math.round(fraction.slice(0, 4).padEnd(4, "0") / 100)
+      fraction = String(
+        Math.round(
+          parseInt(fraction.slice(0, 4).padEnd(4, "0"), 10) / 100,
+        ),
+      )
     }
+
     return (
       <div
         css={({ fonts }) => ({
