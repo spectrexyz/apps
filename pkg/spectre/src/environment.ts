76bc4ebd087fd7d29ec434bccfee921663726bfd
Pierre Bertet <hello@pierre.world>
2022-09-12 14:00:30 +0100

    spectre: validate env with zod

--- pkg/spectre/src/environment.ts
+++ pkg/spectre/src/environment.ts
@@ -1,24 +1,39 @@
-export const CHAIN_ID = Number(import.meta.env.VITE_CHAIN_ID ?? 1)
+import { ADDRESS_RE } from "moire"
+import { z } from "zod"
 
-export const DEMO_MODE = Boolean(
-  parseInt(
-    String(import.meta.env.VITE_DEMO_MODE) || "0",
-    10,
-  ),
-)
+const env = z.object({
+  VITE_CHAIN_ID: z.string().min(1).transform((value, ctx) => {
+    const chainId = parseInt(value, 10)
 
-export const INFURA_PROJECT_ID = String(
-  import.meta.env.VITE_INFURA_PROJECT_ID,
-) || null
+    if (isNaN(chainId) || chainId <= 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "VITE_CHAIN_ID is not a number greater than 0",
+      })
+      return z.NEVER
+    }
 
-export const SUBGRAPH_ENDPOINT = String(
-  import.meta.env.VITE_SUBGRAPH_ENDPOINT,
-) || null
+    return chainId
+  }),
+  VITE_DEMO_MODE: z
+    .union([z.literal("0"), z.literal("1")])
+    .default("0")
+    .transform((value) => Boolean(parseInt(String(value), 10))),
+  VITE_INFURA_PROJECT_ID: z.string().min(1),
+  VITE_SUBGRAPH_ENDPOINT: z.string().url(),
+  VITE_NFT_STORAGE_KEY: z.string().min(1),
+  VITE_CHANNELER_ADDRESS: z.string().regex(ADDRESS_RE),
+  VITE_SERC721_ADDRESS: z.string().regex(ADDRESS_RE),
+  VITE_ERC1155_ADDRESS: z.string().regex(ADDRESS_RE),
+}).parse(import.meta.env)
 
-export const NFT_STORAGE_KEY = String(
-  import.meta.env.VITE_NFT_STORAGE_KEY,
-) || null
-
-export const CHANNELER_ADDRESS = String(import.meta.env.VITE_CHANNELER_ADDRESS)
-export const SERC721_ADDRESS = String(import.meta.env.VITE_SERC721_ADDRESS)
-export const ERC1155_ADDRESS = String(import.meta.env.VITE_ERC1155_ADDRESS)
+export const {
+  VITE_CHAIN_ID: CHAIN_ID,
+  VITE_DEMO_MODE: DEMO_MODE,
+  VITE_INFURA_PROJECT_ID: INFURA_PROJECT_ID,
+  VITE_SUBGRAPH_ENDPOINT: SUBGRAPH_ENDPOINT,
+  VITE_NFT_STORAGE_KEY: NFT_STORAGE_KEY,
+  VITE_CHANNELER_ADDRESS: CHANNELER_ADDRESS,
+  VITE_SERC721_ADDRESS: SERC721_ADDRESS,
+  VITE_ERC1155_ADDRESS: ERC1155_ADDRESS,
+} = env
